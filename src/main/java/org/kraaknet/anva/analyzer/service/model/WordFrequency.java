3c60b79c02670853a7cb666d6c667aaeb777b838
Floris Kraak <randakar@gmail.com>
2024-06-17 09:35:27 +0200

    Add the WordFrequency interface

---
+++ src/main/java/org/kraaknet/anva/analyzer/service/model/WordFrequency.java
@@ -0,0 +1,11 @@
+package org.kraaknet.anva.analyzer.service.model;
+
+public interface WordFrequency {
+
+    // Note - this deviates from the 'getter' style interface provided in the assignment.
+    // The reason is that java Records use a slightly different style, and instead of having
+    // delegations calling these, I've short-circuited it to match.
+    String word();
+    int frequency();
+
+}
