37fdaa5988104136f5f177db337363a4474f7113
Floris Kraak <randakar@gmail.com>
2024-06-17 11:03:04 +0200

    Implement calculateHighestFrequency

--- src/main/java/org/kraaknet/anva/analyzer/service/WordFrequencyAnalyzerService.java
+++ src/main/java/org/kraaknet/anva/analyzer/service/WordFrequencyAnalyzerService.java
@@ -3,13 +3,32 @@ package org.kraaknet.anva.analyzer.service;
 import org.kraaknet.anva.analyzer.service.analyzers.WordFrequencyAnalyzer;
 import org.kraaknet.anva.analyzer.service.model.WordFrequency;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.counting;
 
 public class WordFrequencyAnalyzerService implements WordFrequencyAnalyzer {
 
+    private static final String WORD_EXPRESSION = "[a-zA-Z]+";
+    public static final Pattern WORD_PATTERN = Pattern.compile(WORD_EXPRESSION);
+
     @Override
     public int calculateHighestFrequency(final String text) {
-        return 0;
+        final var matcher = WORD_PATTERN.matcher(text);
+        final Map<String, Long> frequencyMap = matcher.results()
+                .map(MatchResult::group)
+                .collect(Collectors.groupingByConcurrent(word -> word, counting()));
+        return frequencyMap.isEmpty() ? 0 : extractMaxFrequency(frequencyMap).intValue();
+    }
+
+    private Long extractMaxFrequency(final Map<String, Long> frequencyMap) {
+        final String maxWord = Collections.max(frequencyMap.entrySet(), Map.Entry.comparingByValue()).getKey();
+        return frequencyMap.getOrDefault(maxWord, 0L);
     }
 
     @Override
--- src/test/java/org/kraaknet/anva/analyzer/service/WordFrequencyAnalyzerServiceTest.java
+++ src/test/java/org/kraaknet/anva/analyzer/service/WordFrequencyAnalyzerServiceTest.java
@@ -2,17 +2,26 @@ package org.kraaknet.anva.analyzer.service;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.kraaknet.anva.analyzer.service.util.FileUtilities.loadTextFromFile;
 
 class WordFrequencyAnalyzerServiceTest {
 
     private final String loremIpsumText = loadTextFromFile("lorem-ipsum.txt");
+    private final WordFrequencyAnalyzerService service = new WordFrequencyAnalyzerService();
 
     @Test
     void calculateHighestFrequencyTest() {
-        assertNotNull(loremIpsumText);
+        final int result = service.calculateHighestFrequency(loremIpsumText);
+        assertEquals(10, result); // The word "ac" in the lorem ipsum text occurs 10 times.
     }
+
+    @Test
+    void calculateHighestFrequencyTestEmptyText() {
+        final int result = service.calculateHighestFrequency("");
+        assertEquals(0, result); // No text means 0 is the highest frequency.
+    }
+
 
     @Test
     void calculateFrequencyForWord() {
